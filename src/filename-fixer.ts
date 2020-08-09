d11e8f099f54aeff02206b207dd9ad8a0cfebb0a
sharklasers996 <sharklasers996@gmail.com>
2020-08-09 06:24:38 +0300

    Fix filename: added support for interface and enum

--- src/filename-fixer.ts
+++ src/filename-fixer.ts
@@ -1,40 +1,59 @@
-import * as vscode from 'vscode';
-import * as os from 'os';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as os from "os";
+import * as path from "path";
 
 export class FilenameFixer {
-    public async fix() {
-        const document = vscode.window.activeTextEditor?.document;
-        if (typeof document === 'undefined') {
-            return;
-        }
+	public async fix() {
+		const document = vscode.window.activeTextEditor?.document;
+		if (typeof document === "undefined") {
+			return;
+		}
 
-        let classRegex = new RegExp(/class\s(\w+)/g);
-        let className = '';
-        for (let i = 0; i < document!.lineCount; i++) {
-            let documentLine = document?.lineAt(i);
-            let match;
-            if ((match = classRegex.exec(documentLine!.text))) {
-                className = match[1];
-                break;
-            }
-        }
+		const regexes = [
+			/class\s(\w+)/g,
+			/interface\s(\w+)/g,
+			/enum\s(\w+)/g,
+		];
 
-        if (className === '') {
-            vscode.window.showInformationMessage('Couldn\'t find class');
-            return;
-        }
+		let destinationFilename = "";
 
-        var pathSeparator = '/';
-        if (os.platform() === "win32") {
-            pathSeparator = '\\';
-        }
+		for (let i = 0; i < regexes.length; i++) {
+			let regex = new RegExp(regexes[i]);
+			for (let i = 0; i < document!.lineCount; i++) {
+				let documentLine = document?.lineAt(i);
+				let match;
+				if ((match = regex.exec(documentLine!.text))) {
+					destinationFilename = match[1];
+					break;
+				}
+			}
 
-        let destination = path.dirname(document.fileName) + pathSeparator + className + '.cs';
-        if (destination !== document.fileName) {
-            let sourceUri = vscode.Uri.file(document.fileName);
-            let destinationUri = vscode.Uri.file(destination);
-            vscode.workspace.fs.rename(sourceUri, destinationUri);
-        }
-    }
+			if (destinationFilename !== "") {
+				break;
+			}
+		}
+
+		if (destinationFilename === "") {
+			vscode.window.showInformationMessage(
+				"Couldn't find class/interface/enum"
+			);
+			return;
+		}
+
+		var pathSeparator = "/";
+		if (os.platform() === "win32") {
+			pathSeparator = "\\";
+		}
+
+		let destination =
+			path.dirname(document.fileName) +
+			pathSeparator +
+			destinationFilename +
+			".cs";
+		if (destination !== document.fileName) {
+			let sourceUri = vscode.Uri.file(document.fileName);
+			let destinationUri = vscode.Uri.file(destination);
+			vscode.workspace.fs.rename(sourceUri, destinationUri);
+		}
+	}
 }
